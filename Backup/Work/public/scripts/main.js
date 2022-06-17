7cf0ddd5305a4e17f0e81c2b7261b752987e37f9
I-vac <ivo_nikolov00@abv.bg>
2022-06-17 15:17:54 +0300

    worked on the carousel for images in the pdp page

--- Backup/Work/public/scripts/main.js
+++ Backup/Work/public/scripts/main.js
@@ -1,15 +1,47 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
 const minusButton = document.getElementById('minus');
 const plusButton = document.getElementById('plus');
 const inputField = document.getElementById('input');
+const arrowUP = document.getElementById('arrowUP');
+const arrowDOWN = document.getElementById('arrowDOWN');
+const image = document.getElementById('image');
+const imgRef = document.getElementsByClassName('active')[0];
+let currentIndex = imgRef.dataset.index;
 
-minusButton.addEventListener('click', event => {
+
+console.log(currentIndex);
+
+minusButton.addEventListener('click', (event) => {
   event.preventDefault();
   const currentValue = Number(inputField.value) || 0;
   inputField.value = currentValue - 1;
 });
 
-plusButton.addEventListener('click', event => {
+plusButton.addEventListener('click', (event) => {
   event.preventDefault();
   const currentValue = Number(inputField.value) || 0;
   inputField.value = currentValue + 1;
+});
+
+arrowUP.addEventListener('click', (event) => {
+  event.preventDefault();
+  currentIndex = Number(currentIndex);
+  currentIndex -= 1;
+  console.log(currentIndex);
+});
+
+arrowDOWN.addEventListener('click', (event) => {
+  event.preventDefault();
+  currentIndex = Number(currentIndex);
+  currentIndex += 1;
+  console.log(currentIndex);
+});
+
+image.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+$('.activeSize').click(function () {
+  $(this).addClass('selected').siblings().removeClass('selected');
 });
