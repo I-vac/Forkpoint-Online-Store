0a3e5c9cce5acc0e9206ebd6e8554e4387ac7ece
I-vac <ivo_nikolov00@abv.bg>
2022-07-05 13:24:21 +0300

    created the filters and refractored them to be inside the controller instead the ejs file

--- Backup/Work/routes/products.js
+++ Backup/Work/routes/products.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const _ = require('underscore');
 const mdbClient = require('mongodb').MongoClient;
 
@@ -8,6 +9,35 @@ module.exports = function routeCategory(req, res) {
     const collection = db.collection('products');
 
     collection.find({ primary_category_id: result }).toArray((collErr, items) => {
+      const filters = {
+        sizes: [],
+        swatches: [],
+      };
+
+      items.forEach((product) => {
+        product.image_groups.forEach((image_group) => {
+          if (image_group.view_type === 'swatch' && !!image_group.variation_value) {
+            image_group.images.forEach((link) => {
+              if (link.link) {
+                filters.swatches.push(link.link);
+              }
+            });
+          }
+        });
+      });
+
+      items.forEach((product) => {
+        product.variation_attributes.forEach((variations) => {
+          if (variations.id === 'size') {
+            variations.values.forEach((value) => {
+              if (value.orderable === true && !filters.sizes.includes(value.value)) {
+                filters.sizes.push(value.value);
+              }
+            });
+          }
+        });
+      });
+
       res.render('products', {
         // Underscore.js lib
         _,
@@ -15,6 +45,7 @@ module.exports = function routeCategory(req, res) {
         // Template data
         title: 'products',
         items,
+        filters,
       });
       client.close();
     });
