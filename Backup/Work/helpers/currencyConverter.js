961bfb9157e3e7a058e10724aa5afb6e0a0541f2
unknown <ivo_nikolov00@abv.bg>
2023-06-22 21:06:41 +0300

    changed the api for currency exchange

--- Backup/Work/helpers/currencyConverter.js
+++ Backup/Work/helpers/currencyConverter.js
@@ -4,53 +4,31 @@
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-shadow */
-const soap = require('soap');
+const axios = require('axios');
 
 module.exports = function Converter(price) {
-  const url = 'http://infovalutar.ro/curs.asmx?wsdl';
-  let currency;
+  const apiUrl = 'https://v6.exchangerate-api.com/v6/479a19e99fb2bc16910d8500/latest/USD';
   const currencies = ['USD', 'GBP', 'EUR', 'BGN', 'TRY'];
-  const HOURS_OFFSET = 3;
-  const currencyPromise = new Promise((resolve, reject) => soap.createClientAsync(url).then((client) => {
-    client.lastdateinsertedAsync({
-      lastdateinserted: '',
-    }, (err, result) => {
-      if (err) {
-        return reject(err);
+
+  const currencyPromise = axios.get(apiUrl).then((response) => {
+    const { conversion_rates: rates } = response.data;
+
+    if (!rates) {
+      throw new Error('Failed to retrieve exchange rates.');
+    }
+
+    const convertedPrices = {};
+
+    currencies.forEach((currency) => {
+      if (currency !== 'USD' && rates[currency]) {
+        const rate = rates[currency];
+        const convertedPrice = Math.floor(price * rate);
+        convertedPrices[currency] = convertedPrice;
       }
-      const formatDate = result.lastdateinsertedResult;
-      formatDate.setHours(formatDate.getHours() + HOURS_OFFSET);
-      const date = formatDate.toISOString().slice(0, -5);
-
-      client.getallAsync({
-        dt: date,
-      }, async (err, result) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const data = result;
-        const query = data.getallResult.diffgram.DocumentElement.Currency
-          .reduce((obj, value) => {
-            const key = value.IDMoneda;
-
-            if (currencies.includes(key)) {
-              obj[key] = Number(value.Value);
-            }
-            return obj;
-          }, {});
-        currency = await query;
-        const convertedPrices = {};
-        let newPrice;
-        const baseConvert = Math.floor(price * currency.USD);
-
-        Object.entries(currency).forEach(([key, value]) => {
-          newPrice = Math.floor(baseConvert / `${value}`);
-          convertedPrices[key] = newPrice;
-        });
-        return resolve(convertedPrices);
-      });
     });
-  }));
+
+    return convertedPrices;
+  });
+
   return currencyPromise;
 };
